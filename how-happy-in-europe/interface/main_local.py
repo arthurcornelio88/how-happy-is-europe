62c485d58d4e6476d51879e4f376c81d97342cb3
arthurcornelio <arthur.cornelio@gmail.com>
2024-03-21 16:02:20 +0100

    main_local.py baseline-model working

--- how-happy-in-europe/interface/main_local.py
+++ how-happy-in-europe/interface/main_local.py
@@ -7,20 +7,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler, FunctionTransformer, OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.pipeline import Pipeline
 
 # import linear regression model
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
 
 
 # Row for prediction
-X_PRED = pd.DataFrame([np.array(["FR",1,1,1,1,1,6,1,1,1])], columns=['cntry', \
+X_PRED = pd.DataFrame([np.array(["FR",1,1,1,1,1,6,1,1,1,1])], columns=['cntry', \
     'gndr', 'sclmeet', 'inprdsc', 'sclact', 'health', 'rlgdgr','dscrgrp',     \
-    'ctzcntr', 'brncntr'])
+    'ctzcntr', 'brncntr', 'happy'])
 
 # TODO: 0-3 extremely unhappy, 4-7:neutral, 8-10:extremely happy
 STATE_OF_HAPPINESS = [
@@ -36,6 +34,13 @@ STATE_OF_HAPPINESS = [
     "Extremely happy",
     "Extremely happy"
 ]
+
+# TODO:
+# def getting_best_features():
+#     return df_best_features
+
+# from sklearn.linear_model import Ridge
+
 def data_cleaning(df):
 
     # remove the first column
@@ -46,91 +51,66 @@ def load_data():
 
     #load the basemodel dataset
     df = pd.read_csv("arthurcornelio88-notebooks/20240319_ESS10_baseline-model_arthurcornelio88.csv")
-
     return df
 
-def preprocess(clean_data:pd.DataFrame, pred:int, cleaned_full_data:pd.DataFrame=None)-> np.array:
+def num_replacer(df): #, col):
+    for col in df.columns:
+        df_transformed = df.copy()
+        df_transformed[f'{col}_refusal'] = df_transformed[col].apply(lambda x: 1 if x == 77 else -1)
+        df_transformed[f'{col}_dontknow'] = df_transformed[col].apply(lambda x: 1 if x == 88 else -1)
+        df_transformed[f'{col}_noanswer'] = df_transformed[col].apply(lambda x: 1 if x == 99 else -1)
+        # Replace values 77, 88, 99 with -1
+        df_transformed[col] = df_transformed[col].replace([77, 88, 99], -1)
+    return df_transformed
 
-    # TODO : can I do this ? I`m just replacing the row by all dataset but I want to treat row with the columns from the big one
-    #get dummies...
-    if pred == 1 :
-        clean_data = clean_data
-    elif pred == 0:
-        clean_data = cleaned_full_data
+def pipe_preprocess(clean_data:pd.DataFrame)-> np.array:
 
-    X = clean_data
-    #One hot encoding of cntry, ctzcntr, brncntr, gndr, dscrgrp
-    X = pd.get_dummies(X, columns=['cntry'], drop_first=True)
-    X = pd.get_dummies(X, columns=['ctzcntr'], drop_first=True)
-    X = pd.get_dummies(X, columns=['brncntr'], drop_first=True)
-    X = pd.get_dummies(X, columns=['gndr'], drop_first=True)
-    X = pd.get_dummies(X, columns=['dscrgrp'], drop_first=True)
+    # Defining transformers (numerical and categorical)
+    num_replacer_transformer = FunctionTransformer(num_replacer)
+    cat_transformer = OneHotEncoder(handle_unknown='ignore',drop='first', sparse_output=False)
+    # Pipeline for processing data (preproc)
+    preproc = make_column_transformer(
+        (num_replacer_transformer, ['sclmeet','inprdsc','health', 'rlgdgr']),
+        (cat_transformer, ['cntry','ctzcntr','brncntr','gndr', 'dscrgrp']),
+        remainder='passthrough')
+    # creating dataset processed (X_preproc)
+    X_preproc = preproc.fit_transform(clean_data)
 
-    # For sclmeet, create a new column called sclmeet_refusal, sclmeet_dontknow, sclmeet_noanswer , put true if the value is 77, 88, 99 respectively
-    X['sclmeet_refusal'] = X['sclmeet'].apply(lambda x: 1 if x == 77 else 0)
-    X['sclmeet_dontknow'] = X['sclmeet'].apply(lambda x: 1 if x == 88 else 0)
-    X['sclmeet_noanswer'] = X['sclmeet'].apply(lambda x: 1 if x == 99 else 0)
-    # Replace values 77, 88, 99 with O
-    X['sclmeet'] = X['sclmeet'].replace([77, 88, 99], 0)
-
-    # Same for inprdsc
-    X['inprdsc_refusal'] = X['inprdsc'].apply(lambda x: 1 if x == 77 else 0)
-    X['inprdsc_dontknow'] = X['inprdsc'].apply(lambda x: 1 if x == 88 else 0)
-    X['inprdsc_noanswer'] = X['inprdsc'].apply(lambda x: 1 if x == 99 else 0)
-    # Replace values 77, 88, 99 with O
-    X['inprdsc'] = X['inprdsc'].replace([77, 88, 99], 0)
-
-    # Same for health
-    X['health_refusal'] = X['health'].apply(lambda x: 1 if x == 77 else 0)
-    X['health_dontknow'] = X['health'].apply(lambda x: 1 if x == 88 else 0)
-    X['health_noanswer'] = X['health'].apply(lambda x: 1 if x == 99 else 0)
-    # Replace values 77, 88, 99 with O
-    X['health'] = X['health'].replace([77, 88, 99], 0)
-
-    # Same for rlgdgr
-    X['rlgdgr_refusal'] = X['rlgdgr'].apply(lambda x: 1 if x == 77 else 0)
-    X['rlgdgr_dontknow'] = X['rlgdgr'].apply(lambda x: 1 if x == 88 else 0)
-    X['rlgdgr_noanswer'] = X['rlgdgr'].apply(lambda x: 1 if x == 99 else 0)
-    # Replace values 77, 88, 99 with O
-    X['rlgdgr'] = X['rlgdgr'].replace([77, 88, 99], 0)
-
-    #Replace all True with 1 and False with 0
-    X = X.replace([True, False], [1, 0])
-    df_processed = X
-
-    print("✅ preprocess_and_train() done")
-
-    return df_processed
+    print("✅ preprocess() done")
+    return preproc, X_preproc
 
 def train(df_processed):
     # no need for evaluation because it was already done in notebook experimentation
     #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    X, y = df_processed.iloc[:, :-1], df_processed.iloc[:, -1]
+    X, y = df_processed[:, :-1], df_processed[:, -1]
     # Training the linear regression model
     model = LinearRegression()
     model = model.fit(X, y)
 
+    print("✅ train() done")
     return model
 
 def pred(X_pred):
 
     df = load_data()
     df_cleaned = data_cleaning(df)
-    breakpoint()
-    X_preproc = preprocess(df_cleaned,1)
+    #df_best = getting_best_features(df_cleaned)
 
-    # Making predictions
+    # Creating pipeline for processing data (preproc) and
+        # creating dataset processed (X_preproc)
+    preproc, X_preproc = pipe_preprocess(df_cleaned)
+
+    # Training model
     model = train(X_preproc)
 
-    #preprocessing the survey-answers to be predicted (one row, one person)
-    #with the full dataset and then just taking
-    x_pred_preproc = preprocess(X_pred,0,df_cleaned).iloc[:,:-1]
+    # Processing the test data
+    x_pred_preproc = preproc.transform(X_pred)
 
-    #y_pred = model.predict(x_pred_preproc)
-    y_pred = model.predict(X_PRED)
+    # Making the prediction
+    y_pred = model.predict(x_pred_preproc[:, :-1])[0]
 
     # Rounding the predictions to the nearest integer and constraining them to the range [0, 10]
-    y_pred_constrained = np.clip(np.round(y_pred), 0, 10)
+    y_pred_constrained = int(np.clip(np.round(y_pred), 0, 10))
 
     print(y_pred_constrained)
     print(f"You are {STATE_OF_HAPPINESS[y_pred_constrained]}." )
