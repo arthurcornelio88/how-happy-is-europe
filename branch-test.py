a41224d6a5426e50416be7bf7f3941c9f0dda5a6
Mehrshad Shobeiri <mehrshad.shobeiri@gmail.com>
2024-03-26 20:26:58 +0100

    cleaned the repo

--- branch-test.py
+++
@@ -1,4 +0,0 @@
-print("Hello, I'm Arthur! And you?")
-print("Hi, Bhav here")
-print("hey, Mehrshas was also here")
-print('hello i'm kay!')
--- ml_logic/data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-def load_data(filepath):
-    """Loads data from a file path."""
-    df = pd.read_csv(filepath)
-    # Add any initial cleaning steps here
-    return df
